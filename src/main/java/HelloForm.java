e8c530add8322617b19820831529dc2902c05b6b
StarrynightW <XiaoDai130133>
2020-10-27 18:08:28 +0800

    testServlet 第一次提交

---
+++ src/main/java/HelloForm.java
@@ -0,0 +1,41 @@
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class HelloForm extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    public HelloForm(){
+
+        super();
+    }
+
+    protected  void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+        response.setContentType("text/html;charset=UTF-8");
+
+        PrintWriter out = response.getWriter();
+        String title = "使用GET方法读取表单数据";
+        //处理中文
+        String name = new String(request.getParameter("name").getBytes("ISO-8859-1"),"UTF-8");
+        String docType = "<!DOCTYPE html> \n";
+        out.println(docType +
+                "<html>\n" +
+                "<head><title>" + title + "</title></head>\n" +
+                "<body bgcolor=\"#f0f0f0\">\n" +
+                "<h1 align=\"center\">" + title + "</h1>\n" +
+                "<ul>\n" +
+                "  <li><b>站点名</b>："
+                + name + "\n" +
+                "  <li><b>网址</b>："
+                + request.getParameter("url") + "\n" +
+                "</ul>\n" +
+                "</body></html>");
+    }
+
+    public void doPost(HttpServletRequest request,HttpServletResponse response) throws ServletException,IOException{
+        doGet(request,response);
+    }
+}
